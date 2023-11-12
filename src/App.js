fe5aac02dcccc945e7c7d6b7ae704f661c3fdf76
Patrick Lee <zpatricklee@gmail.com>
2023-11-11 19:07:19 -0800

    added super basic navbar using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/HomePage.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Crosslife website under construction</p>
-      </header>
+      <HomePage />
     </div>
   );
 }
